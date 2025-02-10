7eb9a8d4780b8355d35b07d2ba6a66848f6f64ae
Roman <roman.xsfuo@gmail.com>
2025-02-10 15:30:10 +0300

    4fix

--- src/app/new-task/page.tsx
+++ src/app/new-task/page.tsx
@@ -2,17 +2,24 @@
 
 import CaptureForm from "@/components/CaptureForm/CaptureForm";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const NewTask = () => {
+  const [token, setToken] = useState<string | null>(null);
   const router = useRouter();
 
   useEffect(() => {
     const savedToken = localStorage.getItem("taskbot_token");
-    if (!savedToken) {
+    if (savedToken) {
+      setToken(savedToken);
+    } else {
       router.push("/auth");
     }
   }, [router]);
+
+  if (token === null) {
+    return null;
+  }
 
   return (
     <div>
